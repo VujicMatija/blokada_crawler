ce2dfe1c396c8a2c07543631d0bebe957de7bd6e
VujicMatija <vujicmatija04@gmail.com>
2025-01-15 15:31:11 +0100

    uradjena prva verzija

---
+++ proba/proba/spiders/pauk.py
@@ -0,0 +1,30 @@
+from scrapy.shell import Shell
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+from scrapy.commands.shell import Command
+import os
+
+KLJUCNE_RECI = ["student", "blokada", "incident", "referendum", "krv", "protest", "glumci", "mladi", "11:52", "zahtev", "štrajk", "sindikat", "prosveta", "fakultet"]
+linkovi = set()
+
+class CrawlingSpider(CrawlSpider):
+    name = 'pauk'
+    l = ""
+    allowed_domains = ['n1info.rs', 'informer.rs', "rts.rs"]
+    start_urls = ['http://n1info.rs/', 'http://informer.rs/', "https://www.rts.rs/" ]
+    rules = [
+        Rule(LinkExtractor(allow='/vesti', deny=["/?comments"]), callback='parse'),
+        Rule(LinkExtractor(allow='/politika'), callback='parse'),
+    ]
+
+
+    def parse(self, response):
+        for rec in KLJUCNE_RECI:
+            if rec in response.url.lower() and "?comments" not in response.url.lower() :
+                l = response.url.replace("HtmlResponse 200", "")
+                yield {
+                    "link" : response.url.replace("HtmlResponse 200", "")
+                }
+
+
+
